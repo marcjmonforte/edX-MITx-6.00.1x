7cf57c618466269a8ba88d216fff93aeb235db37
Marc Monforte <marcjmonforte@gmail.com>
2017-07-08 17:14:59 -0700

    Completed: ps4a.py; Scrabble game functional

--- Week 4/ps4a.py
+++ Week 4/ps4a.py
@@ -308,17 +308,38 @@ def playHand(hand, wordList, n):
 def playGame(wordList):
     """
     Allow the user to play an arbitrary number of hands.
-
+ 
     1) Asks the user to input 'n' or 'r' or 'e'.
       * If the user inputs 'n', let the user play a new (random) hand.
       * If the user inputs 'r', let the user play the last hand again.
       * If the user inputs 'e', exit the game.
       * If the user inputs anything else, tell them their input was invalid.
  
-    2) When done playing the hand, repeat from step 1    
+    2) When done playing the hand, repeat from step 1
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this line when you code the function
+    # Constant values.
+    hand = []
+    
+    # Ask the user to input 'n' or 'r' or 'e'.
+    while True:    
+        gameStatus = input('Enter n to deal a new hand, r to replay the last hand, or e to end game: ')
+        
+        if gameStatus == 'n':
+            hand = dealHand(HAND_SIZE)
+            playHand(hand, wordList, HAND_SIZE)
+            
+        elif gameStatus == 'r' and hand == []:
+            print("You have not played a hand yet. Please play a new hand first!")
+        
+        elif gameStatus == 'r' and hand != []:
+            playHand(hand, wordList, HAND_SIZE)
+        
+        elif gameStatus == 'e':
+            break
+        
+        else:
+            print('Invalid command.')
+            continue
    
 
 
